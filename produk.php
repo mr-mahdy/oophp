4447d70a45c9d50cd18f619bc918d101832d3ee5
mr-mahdy <mr.mahdy5@gmail.com>
2019-09-25 21:01:56 +0700

    mengisi nilai awal property pada object 1 dan 2 serta mengisi property secara manual

--- produk.php
+++ produk.php
@@ -2,11 +2,17 @@
 
 class Produk
 {
-    public $judul,
-        $penulis,
-        $penerbit,
-        $harga;
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 }
 
 $produk1 = new Produk();
+$produk1->judul = "Naruto";
 var_dump($produk1);
+
+$produk2 = new Produk();
+$produk2->judul = "Uncharted";
+$produk2->tambahProperty = "hahaha";
+var_dump($produk2);
