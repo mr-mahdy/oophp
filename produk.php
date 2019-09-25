2a47e4005667c22ecf97c4b1a407e356e0dd4ba5
mr-mahdy <mr.mahdy5@gmail.com>
2019-09-25 20:54:09 +0700

    menambah property pada class produk dan menghapus produk2

--- produk.php
+++ produk.php
@@ -1,11 +1,12 @@
 <?php
 
-// Jualan Produk
-// Komik
-// Game
-
 class Produk
-{ }
+{
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga;
+}
 
 $produk1 = new Produk();
-$produk2 = new Produk();
+var_dump($produk1);
