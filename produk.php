ddad028d2c5d787b0780e67c617d2e3d171debbd
mr-mahdy <mr.mahdy5@gmail.com>
2019-09-25 21:17:56 +0700

    membuat method pada class produk dan memanggilnya

--- produk.php
+++ produk.php
@@ -6,6 +6,11 @@ class Produk
         $penulis = "penulis",
         $penerbit = "penerbit",
         $harga = 0;
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
 // $produk1 = new Produk();
@@ -23,4 +28,13 @@ $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
 
-echo "Komik : $produk3->judul, Penerbit : $produk3->penerbit";
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
