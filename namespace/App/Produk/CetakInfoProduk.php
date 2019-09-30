c41516c4a801e2e8828dfc1c5f0d062d95220706
mr-mahdy <mr.mahdy5@gmail.com>
2019-09-30 22:33:01 +0700

    menambahkan file namespace

---
+++ namespace/App/Produk/CetakInfoProduk.php
@@ -0,0 +1,20 @@
+<?php
+
+class CetakInfoProduk
+{
+    public $daftarProduk = array();
+
+    public function tambahProduk(Produk $produk)
+    {
+        $this->daftarProduk[] = $produk;
+    }
+
+    public function cetak()
+    {
+        $str = "DAFTAR PRODUK : <br>";
+        foreach ($this->daftarProduk as $produk) {
+            $str .= "- {$produk->getInfoProduk()} <br>";
+        }
+        return $str;
+    }
+}
---
+++ namespace/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+// $cetakProduk = new CetakInfoProduk;
+
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+
+// echo $cetakProduk->cetak();
+
+use App\Produk\User as ProdukUser;
+use App\Service\User as ServiceUser;
+use App\Service\User;
+
+new ProdukUser;
+echo "<br>";
+new ServiceUser;
